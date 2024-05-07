c8f0f9f49d251049241f2bfa8d02f5a98c011d7a
rodasistemas <rodasistemas@gmail.com>
2024-05-07 13:57:04 -0300

    tests

---
+++ __tests__/home.test.tsx
@@ -0,0 +1,17 @@
+import {render, screen } from "@testing-library/react";
+import Home from "@/app/page";
+
+describe("Tests Home", ()=>{
+
+    it("Home page should have Template", ()=>{
+        render(<Home />);
+        const templates = screen.getByText("Templates");
+        expect(templates).toBeDefined()
+    })
+    it("Home page should have About Page Link", ()=>{
+        render(<Home />);
+        const homePageLink = screen.getByRole("link",{name: "About Page"});
+        expect(homePageLink).toBeDefined()
+        expect(homePageLink.getAttribute("href")).not.toBe("/home")
+    })
+})
